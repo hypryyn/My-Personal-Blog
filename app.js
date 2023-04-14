f52c687172f3fad4a7fd1985f2c9fa0a8d105377
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-04-14 23:06:07 +0700

    adding pages

--- app.js
+++ app.js
@@ -12,24 +12,34 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/",function (req,res ) {
-  res.send("<h1>Home</h1>");
+app.get("/", function (req, res) {
+  res.render("home", { startingContent: homeStartingContent });
 });
 
+app.get("/about", function (req, res) {
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function (req,res ){
+  res.render("contact", {contactContent: contactContent});
+});
+
+app.get("/compose", function(req,res){
+  res.render("compose")
+})
+
+app.post("/compose", function(req, res){
+  let  = req.body.postTitle
+})
 
 
 
 
 
 
-
-
-
-
-
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
