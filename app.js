84ca0e48b6b2e6b6bc62979217f62cfdf53c47fa
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-04-15 00:00:27 +0700

    adding post funtion

--- app.js
+++ app.js
@@ -15,8 +15,13 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+let posts = [];
+
 app.get("/", function (req, res) {
-  res.render("home", { startingContent: homeStartingContent });
+  res.render("home", 
+  { startingContent: homeStartingContent, 
+    posts: posts 
+  });
 });
 
 app.get("/about", function (req, res) {
@@ -32,8 +37,13 @@ app.get("/compose", function(req,res){
 })
 
 app.post("/compose", function(req, res){
-  let  = req.body.postTitle
-})
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+  posts.push(post);
+  res.redirect("/");
+});
 
 
 
